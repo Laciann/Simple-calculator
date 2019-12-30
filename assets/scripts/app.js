a36ace49422dd06ac88be9b86a4eaa32633d23a2
Laciann <abhishek.acharya@apexcollege.edu.np>
2019-12-30 14:25:29 +0545

    console log object

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = 0;
+let logEntries = [];
 
 //get input from input field
 function getUserNumberInput(){
@@ -12,11 +13,28 @@ function WriteOuput(previousResult, operator, enteredNumber){
     outputResult(currentResult, calcDescription); //function from vendor.js
 }
 
+//writeToLog 
+function writeToLog(OprnIdentifier, prevRes, OprnNum, newRes) {
+   
+    const logEntry = {
+        operation: OprnIdentifier,
+        prevRes: prevRes,
+        number: OprnNum,
+        result: newRes
+    };
+
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+
 function add(){
     const previousResult = currentResult;
     const enteredNumber = getUserNumberInput();
     currentResult +=  parseInt(enteredNumber);
     WriteOuput(previousResult, '+', enteredNumber);
+    logEntries.push(enteredNumber);
+    writeToLog('ADD', previousResult, enteredNumber, currentResult);
 }
 
 
@@ -25,6 +43,7 @@ function subtract(){
     const enteredNumber = getUserNumberInput();
     currentResult -=  parseInt(enteredNumber);
     WriteOuput(previousResult, '-', enteredNumber);
+    writeToLog('SUBTRACT', previousResult, enteredNumber, currentResult);
 }
 
 function multiply(){
@@ -32,6 +51,7 @@ function multiply(){
     const enteredNumber = getUserNumberInput();
     currentResult *=  parseInt(enteredNumber);
     WriteOuput(previousResult, '*', enteredNumber);
+    writeToLog('MULTIPLY', previousResult, enteredNumber, currentResult);
 }
 
 function divide(){
@@ -39,6 +59,7 @@ function divide(){
     const enteredNumber = getUserNumberInput();
     currentResult /=  parseInt(enteredNumber);
     WriteOuput(previousResult, '/', enteredNumber);
+    writeToLog('DIVIDE', previousResult, enteredNumber, currentResult);
 }
 
 
